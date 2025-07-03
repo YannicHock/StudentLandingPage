bc3ef3163c3b89817d05fbddecd7085a8bfbdf31
Yannic Hock <yaho00002@htwsaar.de>
2025-07-03 10:34:15 +0200

    Kalender first draft

--- src/components/card_elements/Calender.tsx
+++ src/components/card_elements/Calender.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from "@fullcalendar/interaction"
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from "@fullcalendar/interaction";
 import allLocales from '@fullcalendar/core/locales-all';
 
 const ModulDatabase: React.FC = () => {
-    const handleDateClick = (arg:any) => {
-        alert(arg.dateStr)
-    }
+    const handleDateClick = (arg: { dateStr: string }) => {
+        alert(arg.dateStr);
+    };
 
-    const handleEventClick = (arg:any) => {
-        console.log(arg)
-    }
+    const handleEventClick = (arg: { event: any }) => {
+        console.log(arg);
+    };
 
     return (
         <FullCalendar
-            plugins={[dayGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView="timeGridWeek"
+            headerToolbar={{
+                left: 'today,prev,next',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            slotMinTime="07:00:00"
+            slotMaxTime="20:00:00"
+            slotDuration="00:30:00"
             weekends={false}
+            weekNumbers={true}
             events={[
-                {title: 'event 1', date: '2025-07-07'},
-                {title: 'event 2', date: '2025-07-08'}
+                {
+                    title: 'event 1',
+                    start: '2025-07-03T10:15:00',
+                    end: '2025-07-03T11:30:00',
+                    description: 'This is the first event',
+                    extendedProps: {
+                        location: 'Berlin',
+                        organizer: 'John Doe'
+                    },
+                    display: "block",
+                },
             ]}
             locales={allLocales}
             locale={'de'}
@@ -31,4 +50,3 @@ const ModulDatabase: React.FC = () => {
 };
 
 export default ModulDatabase;
-
